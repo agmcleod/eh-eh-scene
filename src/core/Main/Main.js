94d869b9a4a511022204cf92a80880896fb6ac41
Aaron McLeod <aaron.g.mcleod@gmail.com>
2019-10-19 13:51:16 -0400

    switching to material design

--- src/core/Main/Main.js
+++ src/core/Main/Main.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
 
 import { renderMap } from 'common/renderMap'
 import { parseMapData } from 'common/parseMapData'
+import { FileInput } from './styledComponents'
 
 export const Main = () => {
   const fileField = React.createRef()
@@ -13,37 +13,38 @@ export const Main = () => {
   return (
     <Container>
       <h1>Level</h1>
-      <Row>
-        <Col>
-          <label htmlFor='file'>
-            Select .tmx & corresponding tileset images
-          </label>
-          <input
-            id='file'
-            type='file'
-            ref={fileField}
-            multiple
-            onChange={() =>
-              parseMapData(fileField, (err, mapData, tilesetImage) => {
-                if (err) {
-                  console.error(err)
-                } else {
-                  const canvas = document.querySelector('#canvas')
-                  canvas.width = mapData.width * mapData.tileWidth
-                  canvas.height = mapData.height * mapData.tileHeight
+      <div>
+        <label htmlFor='file'>Select .tmx & corresponding tileset images</label>
+        <FileInput
+          id='file'
+          type='file'
+          ref={fileField}
+          multiple
+          onChange={() =>
+            parseMapData(fileField, (err, mapData, tilesetImage) => {
+              if (err) {
+                console.error(err)
+              } else {
+                const canvas = document.querySelector('#canvas')
+                canvas.width = mapData.width * mapData.tileWidth
+                canvas.height = mapData.height * mapData.tileHeight
 
-                  const ctx = canvas.getContext('2d')
-                  ctx.fillStyle = '#ccc'
-                  ctx.fillRect(0, 0, canvas.width, canvas.height)
+                const ctx = canvas.getContext('2d')
+                ctx.fillStyle = '#ccc'
+                ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-                  tilesetImage.onload = () =>
-                    renderMap(ctx, mapData, tilesetImage)
-                }
-              })
-            }
-          />
-        </Col>
-      </Row>
+                tilesetImage.onload = () =>
+                  renderMap(ctx, mapData, tilesetImage)
+              }
+            })
+          }
+        />
+        <label htmlFor='file'>
+          <Button variant='contained' component='span'>
+            Upload
+          </Button>
+        </label>
+      </div>
       <canvas id='canvas' ref={canvas} />
     </Container>
   )
